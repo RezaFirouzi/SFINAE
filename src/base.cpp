2858106fb787b4c55e8d206fd0d1a44e572dd7f4
Reza <sr.firouzi@gmail.com>
2024-10-11 14:13:01 +0330

    ported base.cpp example

---
+++ src/base.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+template <typename T>
+struct tModulo
+{
+private:
+    T m_;
+
+public:
+    explicit tModulo(T m = T{}) : m_{m} {}
+
+    T operator%(T m)
+    {
+        return m_ % m;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const tModulo& m)
+    {
+        os << m.m_;
+        return os;
+    }
+};
+
+int main()
+{
+    // // C++14 - Template argument to be provided
+    // tModulo<int> a{5};
+
+    // C++17 - class template argument type deduction
+    tModulo a{5};
+    int b{3};
+    // short b{3};
+
+    std::cout << a << " % " << b << " = " << (a % b) << "\n";
+
+    // tModulo<double> x{5.0};
+    // double y{3.0};
+    //
+    // std::cout << x << "%" << y << " = " << (x % y) << "\n";
+}
